9454c054b970b83684c274d4b348d3162304f695
Dhivya-Kalyanasundaram <dhivya.k1507@gmail.com>
2025-07-12 10:38:38 +0530

    Initial Commit

---
+++ Dhivya_K_Zuper_UseCase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dhivya_K_Zuper_UseCase
+//
+//  Created by Kalyanasundaram, Dhivya (Cognizant) on 12/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dhivya_K_Zuper_UseCase/Dhivya_K_Zuper_UseCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dhivya_K_Zuper_UseCaseApp.swift
+//  Dhivya_K_Zuper_UseCase
+//
+//  Created by Kalyanasundaram, Dhivya (Cognizant) on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Dhivya_K_Zuper_UseCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
